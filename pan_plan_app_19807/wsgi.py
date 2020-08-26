9fa5af32ed20592c50da2603a8b4e47fb87a6a01
Crowdbotics <team@crowdbotics.com>
2020-08-26 23:44:10 +0000

    Update models

--- pan_plan_app_19807/wsgi.py
+++ pan_plan_app_19807/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pan_plan_app_19807.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pan_plan_app_19807.settings")
 
 application = get_wsgi_application()
